af663a1ab12c171d3e02a27954374348c1ab905c
putrinabila <43230324+putrinabila@users.noreply.github.com>
2019-03-12 08:51:20 +0700

    Add files via upload

---
+++ pre test.cpp
@@ -0,0 +1,41 @@
+/*
+Nama : Putri Nabila
+kelas : A
+NPM : 140810180007
+Deskripsi : Pre test 03
+tanggal : 12 Maret 2019
+*/
+
+#include <iostream>
+ using namespace std;
+ 
+
+ struct r_mahasiswa
+{
+	char npm [14];
+	char nama [40];
+	float IPK;
+	
+ mhs[10];
+  
+int main () {
+  int i;
+  cout<<"Data mahasiswa :"<<endl;
+  for (i=1; i<=n; i++) {
+  cout<<" Mahasiswa ke "<<i<<endl;
+  cout<<"Nama :";
+  cin>>mhs[i].nama; 
+
+ cout<<"Masukkan NPM :";
+  cin>>mhs[i].npm;
+  cout<<"Masukkan IPK :";
+  cin>>mhs[i].IPK;}
+  for (i=1; i<=n; i++) {
+  cout<<endl<<endl<<"Mahasiswa  ke- "<<i<<endl;
+  cout<<"Nama :"<<mhs[i].nama<<end;
+  cout<<"NPM :"<<mhs[i].npm<<endl;
+  cout<<"IPK :"<<mhs[i].IPK<<endl;
+  }
+ system("pause");
+ return 0;
+ }
